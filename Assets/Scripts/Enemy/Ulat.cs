09f32a7092b41c5e4bb3b86d821499807271cc88
Taufan Augusta <taufan.augusta@protonmail.com>
2021-09-29 09:34:46 +0700

    added: prefabs (enemies)

--- Assets/Scripts/Enemy/Ulat.cs
+++ Assets/Scripts/Enemy/Ulat.cs
@@ -2,11 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tikus : Enemy
+public class Ulat : Enemy
 {
-  private void Update()
-  {
-    base.Move();
-  }
-  
+
 }
--- Assets/Scripts/Enemy/WalangSangit.cs
+++ Assets/Scripts/Enemy/WalangSangit.cs
@@ -2,11 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tikus : Enemy
+public class WalangSangit : Enemy
 {
-  private void Update()
-  {
-    base.Move();
-  }
-  
+
 }
