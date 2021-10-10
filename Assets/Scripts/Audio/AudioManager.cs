bb590bf00471f1a37cfd09072e2416f2f6703f36
Taufan Augusta <taufan.augusta@protonmail.com>
2021-10-10 11:53:15 +0700

    tested: audio

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    private void Start() 
+    {
+        
+    }
+
+    private void Update() 
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound 
+{
+    public string Name;
+    public AudioClip Clip;
+
+    [Range(0f, 1f)] public float Volume;
+    [Range(0f, 1f)] public float Pitch;
+
+
+}
